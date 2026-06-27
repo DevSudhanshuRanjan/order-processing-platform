b69cd47252c81c314407d8fc2dc890de7ff5f294
Nothinormuch <nothin.main@outlook.com>
2026-06-27 09:44:37 +0530

    feat: add image module to backend

---
+++ backend/src/models/Image.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const imageSchema = new mongoose.Schema(
+  {
+    data: {
+      type: Buffer,
+      required: true,
+    },
+    contentType: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Image = mongoose.model("Image", imageSchema);
+
+export default Image;
